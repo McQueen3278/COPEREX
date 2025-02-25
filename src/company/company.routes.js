0adf4a348069577c94fb4a8c51e091db8e30370b
unknown <hrodriguez-2023278@kinal.edu.gt>
2025-02-24 23:52:26 -0600

    Fix: se corrigen errores en las rutas

--- src/company/company.routes.js
+++ src/company/company.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { registerCompany, getCompanies } from "./company.controller";
-import { registerCompanyValidator } from "../middlewares/company-validator";
+import { registerCompanyValidator, getCompaniesValidator } from "../middlewares/company-validator";
 
 const router = Router();
 
 router.post("/registerCompany", registerCompanyValidator, registerCompany);
 
-router.post("/getCompanies", getCompanies)
+router.post("/getCompanies",getCompaniesValidator,  getCompanies)
 
 export default router;
--- src/middlewares/company-validator.js
+++ src/middlewares/company-validator.js
@@ -6,12 +6,16 @@ import {validateJWT} from "./validate-jwt.js";
 import {hasRoles} from "./role-validator.js";
 
 export const registerCompanyValidator = [
-    validateJWT,
-    hasRoles("ADMIN_ROLE"),
     body("name").not().isEmpty().withMessage("El nombre es obligatorio"),
     body("impact").not().isEmpty().withMessage("El impacto es obligatorio"),
     body("trajectory").not().isEmpty().withMessage("La trayectoria es obligatoria"),
     body("category").not().isEmpty().withMessage("La categoria es obligatoria"),
+    body("name").custom(companyExists),
     validarCampos,
     handleErrors
+]
+
+export const getCompaniesValidator = [
+    validateJWT,
+    hasRoles('ADMIN_ROLE')
 ]
