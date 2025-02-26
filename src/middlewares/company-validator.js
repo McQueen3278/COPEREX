226dafd2e5f5ab1d9415401e5837b14bb588b2a9
hrodriguez <hrodriguez-2023278@kinal.edu.gt>
2025-02-26 13:05:57 -0600

    Se corrige ruta de endPoint

--- src/middlewares/company-validator.js
+++ src/middlewares/company-validator.js
@@ -6,6 +6,8 @@ import {validateJWT} from "./validate-jwt.js";
 import {hasRoles} from "./role-validator.js";
 
 export const registerCompanyValidator = [
+    validateJWT,
+    hasRoles("ADMIN_ROLE"),
     body("name").not().isEmpty().withMessage("El nombre es obligatorio"),
     body("impact").not().isEmpty().withMessage("El impacto es obligatorio"),
     body("trajectory").not().isEmpty().withMessage("La trayectoria es obligatoria"),
